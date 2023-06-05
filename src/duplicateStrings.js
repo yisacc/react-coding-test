1703aba2315855f29709338ae45058c7884c685e
Yisacc Aberham <isaccab2019@gmail.com>
2023-06-05 17:27:18 +0300

    remove duplicate string

--- src/duplicateStrings.js
+++ src/duplicateStrings.js
@@ -6,7 +6,30 @@
  removeDuplicates(str); // 'This is a test string'
  */
 
- const removeDuplicates(str)=>{
-    const wordArray=str.split(" ")
-    
+//  const removeDuplicates = (str) => {
+//    const wordArray = str.split(" ");
+//    let newArrayStr = [];
+//    wordArray.forEach((element) => {
+//      let count = 0;
+//      newArrayStr.forEach((el) => {
+//        if (el === element) {
+//          count++;
+//        }
+//      });
+//      if (count === 0) {
+//        newArrayStr.push(element);
+//      }
+//    });
+//    return newArrayStr.join(" ");
+//  };
+
+ const removeDuplicates = (str) => {
+    const arr=str.split(' ')
+    const set= new Set(arr)
+
+    const newString = [...set].join(" ");
+    return newString;
  }
+
+ const str = "This is is a test test string";
+ console.log(removeDuplicates(str));
